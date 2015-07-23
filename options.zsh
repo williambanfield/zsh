9b859c948a15b06141dd39a66b70c99dda5f74f5
Lucas Morales <lucasem@mit.edu>
2015-07-23 16:25:13 -0400

    keybindings

--- options.zsh
+++ options.zsh
@@ -4,6 +4,18 @@
 # shell options as history size, keyindings, etc
 #
 
+# keybindings Strg+v is your friend :-)
+bindkey "^[[1;5D" .backward-word
+bindkey "^[[1;5C" .forward-word
+bindkey "^[[1;6D" backward-delete-word
+bindkey "^[[1;6C" delete-word
+# alt+left (on mac) deletes word
+bindkey "^[" backward-kill-word
+# fn-left
+bindkey "^[[H" .backward-word
+# fn-right
+bindkey "^[[F" .forward-word
+
 # arrow up/down searches in history if line is already started
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
