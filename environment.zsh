0d74654aa24241aea0dbc9215149cf9456286888
William Banfield <wbanfield@gmail.com>
2020-01-03 16:10:10 -0500

    fix gobin

--- environment.zsh
+++ environment.zsh
@@ -1,7 +1,7 @@
 # golang
 export GOROOT=/usr/lib/go
 export GOPATH=$HOME/godev
-export GOBIN=$GOPATH/godev/bin
+export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
 export PATH=$PATH:$GOPATH/bin
 
