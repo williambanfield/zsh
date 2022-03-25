2bf40bc9299e9e436876484154160065d4730a7d
William Banfield <wbanfield@gmail.com>
2022-03-25 00:17:13 -0400

    use fzf plugin

--- plugins.zsh
+++ plugins.zsh
@@ -1,6 +1,7 @@
 zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
 zinit snippet OMZ::plugins/git/git.plugin.zsh
+zinit snippet OMZ::plugins/fzf/fzf.plugin.zsh
+zinit light Aloxaf/fzf-tab
 
-zinit  load zsh-users/zsh-completions
 zinit  load zdharma/fast-syntax-highlighting
 zinit  load zsh-users/zsh-autosuggestions
